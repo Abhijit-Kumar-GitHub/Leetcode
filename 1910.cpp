1c1b1d1ddeb63d24ad040a45e0c45dbb1d521fd9
FarSpawn <140056029+Abhijit-Kumar-GitHub@users.noreply.github.com>
2025-02-11 16:47:32 +0530

    Created 1910.cpp

---
+++ 1910.cpp
@@ -0,0 +1,23 @@
+// 1910. Remove All Occurrences of a Substring
+
+// https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int partLen = part.size();
+        string result;
+        for (char c : s) {
+            result.push_back(c);
+            if (result.size() >= partLen && result.substr(result.size() - partLen) == part) {
+                result.erase(result.size() - partLen, partLen);
+            } }
+        return result;
+    } };
