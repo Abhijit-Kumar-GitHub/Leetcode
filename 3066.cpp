7a72091f531e5b0dc763c412e32f6583689035da
FarSpawn <140056029+Abhijit-Kumar-GitHub@users.noreply.github.com>
2025-02-13 18:57:28 +0530

    Created 3066.cpp

---
+++ 3066.cpp
@@ -0,0 +1,25 @@
+// 3066. Minimum Operations to Exceed Threshold Value II
+
+// https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/description/
+
+
+
+
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& a, int k) {
+        priority_queue<int, vector<int>, greater<int>> q(a.begin(), a.end());
+        for (int i = 0, x, y; ; ++i) {
+            x = q.top();
+            q.pop();
+            if (x >= k) return i;
+            y = q.top();
+            q.pop();
+            const long t = 2L * x + y; // test overflow
+            q.push(t < k ? (int)t : k);
+        }
+        return -1;
+    }
+};
